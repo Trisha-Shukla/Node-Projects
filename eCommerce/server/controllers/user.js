8664019d05b8c9901e1711b475d7b454ee48ab28
Trisha Shukla <tshukla025@gmail.com>
2025-01-01 16:44:12 +0530

    cookie related update in user.js controller

--- eCommerce/server/controllers/user.js
+++ eCommerce/server/controllers/user.js
@@ -167,7 +167,7 @@ export async function login(request, response) {
     response.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
